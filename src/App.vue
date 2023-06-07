98e2ebe431871145cac9f375be878e2cc8fed02c
sim-sal <simone.saladino93@gmail.com>
2023-06-07 20:00:42 +0200

    created AppSearch component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
-import { store } from '../src/store'
+import { store } from './store'
 import axios from 'axios'
+import AppSearch from './components/AppSearch.vue'
 
 export default {
   components: {
-
+    AppSearch
   },
   data() {
     return {
@@ -22,7 +23,11 @@ export default {
 
 
 
-<template></template>
+<template>
+  <header>
+    <AppSearch />
+  </header>
+</template>
 
 <style lang="scss">
 @use './styles/general.scss';
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,47 @@
+<script>
+import { store } from '../store'
+
+export default {
+    name: "AppSearch",
+}
+</script>
+
+<template>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand"><strong>BOOLFLIX</strong></a>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+</template>
+
+<style scoped lang="scss">
+.navbar {
+    padding-top: 0;
+
+    .container-fluid {
+        background-color: rgb(48, 48, 48);
+
+        a {
+            color: red;
+            font-size: 50px;
+        }
+
+        .d-flex {
+            button {
+                background-color: white;
+                color: black;
+
+                &:hover {
+                    background-color: red;
+                    color: white;
+                }
+            }
+        }
+    }
+
+}
+</style>
